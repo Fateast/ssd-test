571008299ca6dc4c8dee6556fc258f69a2d39cd8
DmNosachev <99CBDXD6RxlYmlFt>
2014-12-29 18:17:07 +0300

    minor update

--- snia-pts-client/test01.sh
+++ snia-pts-client/test01.sh
@@ -71,10 +71,13 @@ do
 	for for ACT_RANGE_AMOUNT in "${ACT_RANGE_AMOUNTS_KiB[@]}"
 	do
 		# Calculate offsets for active zones
-		#purge the device
-		hdparm --user-master u --security-set-pass PasSWorD $1
-		hdparm --user-master u --security-erase PasSWorD $1
-
+		# TODO: add rounding
+		ZONESIZE=echo "$DEV_SIZE / $ACT_ZONES" | bc -l
+		
+		# Purge the device
+		#hdparm --user-master u --security-set-pass PasSWorD $1
+		#hdparm --user-master u --security-erase PasSWorD $1
+		dd if=/dev/zero of=$1 bs=1M
 		echo "$TIMESTAMP Purge done" >> $LOG_FILE
 
 		echo "OIO/thread = $OIO, Threads = $THREADS" >> $LOG_FILE
@@ -86,21 +89,20 @@ do
 
 		$FIO --name=precondition --filename=$1 --iodepth=16 --numjobs=1 --bs=128k --ioengine=libaio --rw=write --group_reporting --direct=1 --thread --refill_buffers --loops=2
 		echo "$TIMESTAMP Preconditioning done" >> $LOG_FILE
-
+		
+		for PASS in $(eval echo {1..$ROUNDS});
+		do
+			for RWMIX in 100 95 65 50 35 5 0;
+			do
+				for BS in 512 4096 8192 16384 32768 65536 131072 1048576;
+				do
+				$FIO --output-format=json --name=job --filename=$1 --zonesize=$ZONESIZE --zonerange=$ZONERANGE --zoneskip=$ZONESKIP --iodepth=$OIO --numjobs=$THREADS --bs=$BS --ioengine=libaio --rw=randrw --rwmixread=$RWMIX --group_reporting --runtime=60 --direct=1 --norandommap --thread --refill_buffers --output=${TEST_NAME}/results/fio_range=${ACT_RANGE}_range_amt=${ACT_RANGE_AMOUNT}_pass=${PASS}_rw=${RWMIX}_bs=${BS}.json
+				clear
+				done
+			done
+		  echo -e "$TIMESTAMP ${TEST_NAME} pass $PASS of $ROUNDS done" >> $LOG_FILE
+		done
 	done
-done
-
-for PASS in $(eval echo {1..$ROUNDS});
-  do
-  for RWMIX in 100 95 65 50 35 5 0;
-    do
-    for BS in 512 4096 8192 16384 32768 65536 131072 1048576;
-      do
-      $FIO --output-format=json --name=job --filename=$1 --iodepth=$OIO --numjobs=$THREADS --bs=$BS --ioengine=libaio --rw=randrw --rwmixread=$RWMIX --group_reporting --runtime=60 --direct=1 --norandommap --thread --refill_buffers --output=${TEST_NAME}/results/fio_pass=${PASS}_rw=${RWMIX}_bs=${BS}.json
-	  clear
-    done
-  done
-  echo -e "$TIMESTAMP ${TEST_NAME} pass $PASS of $ROUNDS done" >> $LOG_FILE
 done
 
 exit 0
